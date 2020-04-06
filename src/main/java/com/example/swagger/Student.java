761026db75a6c487fbc3415fbf83b536a533eb64
Deniz Gürsoy <dgursoy@innova.com.tr>
2020-04-06 17:25:13 +0300

    explenation to model is added

--- src/main/java/com/example/swagger/Student.java
+++ src/main/java/com/example/swagger/Student.java
@@ -1,11 +1,15 @@
 package com.example.swagger;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@ApiModel(value = " Student Class that hold students information")
 public class Student {
+    @ApiModelProperty(value = "Ordered number given by the database")
     private long studentId;
     private String name;
     private String surname;
